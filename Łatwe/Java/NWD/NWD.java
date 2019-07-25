02c3466efb3745a774b470c60020a16b971bfd81
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-25 15:04:20 +0200

    Create NWD.java

---
+++ Łatwe/Java/NWD/NWD.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class NWD {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println(nwd(a, b));
+        }
+    }
+
+    static int nwd(int a, int b) {
+        while (b != 0) {
+            int c = a % b;
+            a = b;
+            b = c;
+        }
+        return a;
+    }
+}
