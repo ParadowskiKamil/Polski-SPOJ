a9a6e8448f4e27b425c0f52051a0d0f229a02cd0
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-25 15:02:18 +0200

    Create LiczbyPierwsze.java

---
+++ Łatwe/Java/Liczby Pierwsze/LiczbyPierwsze.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class LiczbyPierwsze {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            int number = in.nextInt();
+            if (isPrime(number) && number != 1)
+                System.out.println("TAK");
+            else
+                System.out.println("NIE");
+            }
+        }
+
+    static boolean isPrime(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
