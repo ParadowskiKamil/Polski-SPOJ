b2105f075b18370381b79d22a3f24a75dfff2e29
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-26 15:05:17 +0200

    Create DodawanieLiczbCalkowitych.java

---
+++ Łatwe/Java/Dodawanie liczb całkowitych/DodawanieLiczbCalkowitych.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class DodawanieLiczbCalkowitych {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int firstNumber = in.nextInt();
+        int secondNumber = in.nextInt();
+        int thirdNumber = in.nextInt();
+        System.out.print(firstNumber + secondNumber + thirdNumber);
+    }
+}
