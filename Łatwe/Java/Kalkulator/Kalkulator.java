3aa8f8c6bb4de8c558f84cc1d0582951f309d2ab
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-26 15:10:25 +0200

    Create Kalkulator.java

---
+++ Łatwe/Java/Kalkulator/Kalkulator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Kalkulator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            char sign = in.next().charAt(0);
+            int firstNumber = in.nextInt();
+            int secondNumber = in.nextInt();
+            System.out.println(calculateTwoIntegers(sign, firstNumber, secondNumber));
+        }
+    }
+
+    static int calculateTwoIntegers(char sign, int firstNumber, int secondNumber) {
+        switch (sign) {
+            case '+':
+                return firstNumber + secondNumber;
+            case '-':
+                return firstNumber - secondNumber;
+            case '*':
+                return firstNumber * secondNumber;
+            case '/':
+                return firstNumber / secondNumber;
+            default:
+                return firstNumber % secondNumber;
+        }
+    }
+}
