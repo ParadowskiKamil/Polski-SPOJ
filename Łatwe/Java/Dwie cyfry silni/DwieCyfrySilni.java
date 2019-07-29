7a029f8565de9cc0d4113f3a744a60db1ba6f94b
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-29 13:51:12 +0200

    Update DwieCyfrySilni.java

--- Łatwe/Java/Dwie cyfry silni/DwieCyfrySilni.java
+++ Łatwe/Java/Dwie cyfry silni/DwieCyfrySilni.java
@@ -4,11 +4,11 @@ class DwieCyfrySilni {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int D = in.nextInt();
-        String[] twoFirstDigitsOfFirstTenFactorials = {"0 1", "0 1", "0 2", "0 6", "2 4", "2 0", "2 0", "4 0", "2 0", "8 0"};
+        String[] firstTwoDigitsOfFirstTenFactorials = {"0 1", "0 1", "0 2", "0 6", "2 4", "2 0", "2 0", "4 0", "2 0", "8 0"};
         for (int i = 0; i < D; i++) {
             int n = in.nextInt();
             if (n < 10)
-                System.out.println(twoFirstDigitsOfFirstTenFactorials[n]);
+                System.out.println(firstTwoDigitsOfFirstTenFactorials[n]);
             else
                 System.out.println("0 0");
         }
