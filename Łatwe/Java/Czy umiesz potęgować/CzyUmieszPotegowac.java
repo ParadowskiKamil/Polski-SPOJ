0958d21a083848f091a6e2a47976f1fbf91efdbd
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-29 15:12:46 +0200

    Create CzyUmieszPotegowac.java

---
+++ Łatwe/Java/Czy umiesz potęgować/CzyUmieszPotegowac.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class CzyUmieszPotegowac {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int D = in.nextInt();
+        for (int i = 0; i < D; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            printDigitOfUnityOfPower(a, b);
+        }
+    }
+
+    static void printDigitOfUnityOfPower(int a, int b) {
+        int[][] firstFourDigitsOfFirstTenPowers = {
+                {0, 0, 0, 0},
+                {1, 1, 1, 1},
+                {6, 2, 4, 8},
+                {1, 3, 9, 7},
+                {6, 4, 6, 4},
+                {5, 5, 5, 5},
+                {6, 6, 6, 6},
+                {1, 7, 9, 3},
+                {6, 8, 4, 2},
+                {1, 9, 1, 9}
+        };
+        System.out.println(firstFourDigitsOfFirstTenPowers[a % 10][b % 4]);
+    }
+}
