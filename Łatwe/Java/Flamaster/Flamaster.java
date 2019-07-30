486ad9c4696d0925f0169b02176aca5ac239bd3c
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-30 18:30:43 +0200

    Create Flamaster.java

---
+++ Łatwe/Java/Flamaster/Flamaster.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Flamaster {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int C = in.nextInt();
+        for (int i = 0; i < C; i++) {
+            String str = in.next();
+            printReducedString(str);
+        }
+    }
+
+    static void printReducedString(String str) {
+        str += ' ';
+        for (int j = 0; j < str.length() - 1; j++) {
+            int counter = 1;
+            while (str.charAt(j) == str.charAt(j + 1)) {
+                counter++;
+                j++;
+            }
+            System.out.print(str.charAt(j));
+            if (counter > 2)
+                System.out.print(counter);
+            else if (counter == 2)
+                System.out.print(str.charAt(j));
+        }
+        System.out.println();
+    }
+}
