9f3c377b45bf0c2bc72c1af0db449539b8e0b83b
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-31 16:51:05 +0200

    Update CzyUmieszPotegowac.java

--- Łatwe/Java/Czy umiesz potęgować/CzyUmieszPotegowac.java
+++ Łatwe/Java/Czy umiesz potęgować/CzyUmieszPotegowac.java
@@ -11,7 +11,7 @@ class CzyUmieszPotegowac {
         }
     }
 
-    static void printDigitOfUnityOfPower(int a, int b) {
+    public static void printDigitOfUnityOfPower(int a, int b) {
         int[][] firstFourDigitsOfFirstTenPowers = {
                 {0, 0, 0, 0},
                 {1, 1, 1, 1},
