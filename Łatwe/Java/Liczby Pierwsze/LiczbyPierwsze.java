cb26e81439c0163bd8e309349a908e2e2d280d3f
Kamfeth <53303297+Kamfeth@users.noreply.github.com>
2019-07-31 16:45:57 +0200

    Update LiczbyPierwsze.java

--- Łatwe/Java/Liczby Pierwsze/LiczbyPierwsze.java
+++ Łatwe/Java/Liczby Pierwsze/LiczbyPierwsze.java
@@ -6,14 +6,14 @@ class LiczbyPierwsze {
         int n = in.nextInt();
         for (int i = 0; i < n; i++) {
             int number = in.nextInt();
-            if (isPrime(number) && number > 1)
+            if (isPrime(number) && number != 1)
                 System.out.println("TAK");
             else
                 System.out.println("NIE");
             }
         }
 
-    static boolean isPrime(int number) {
+    public static boolean isPrime(int number) {
         for (int i = 2; i < number; i++) {
             if (number % i == 0)
                 return false;
