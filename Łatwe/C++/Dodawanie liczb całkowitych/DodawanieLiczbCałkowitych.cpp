e59674fb3c0e3b817882d2bf1e1e4fc07275be62
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-17 12:43:17 +0200

    Create DodawanieLiczbCałkowitych.cpp

---
+++ Łatwe/C++/Dodawanie liczb całkowitych/DodawanieLiczbCałkowitych.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int firstNumber;
+    int secondNumber;
+    int thirdNumber;
+    std::cin >> firstNumber >> secondNumber >> thirdNumber;
+    std::cout << firstNumber + secondNumber + thirdNumber;
+}
