dba0c47068e651c366c7dfa5c9030d07280c03a6
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-08-24 13:03:43 +0200

    Create NowaDziałka.cpp

---
+++ Łatwe/C++/Nowa działka/NowaDziałka.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int D;
+    std::cin >> D;
+    for (int i = 0; i < D; i++) {
+        int X;
+        std::cin >> X;
+        std::cout << X * X << std::endl;
+    }
+}
