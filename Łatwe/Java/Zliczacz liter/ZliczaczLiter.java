1c322561f0aef44d8083ec82611453f9a0a1b8f1
Kamil <53303297+Kamfeth@users.noreply.github.com>
2019-09-12 22:56:05 +0200

    Update ZliczaczLiter.java

--- Łatwe/Java/Zliczacz liter/ZliczaczLiter.java
+++ Łatwe/Java/Zliczacz liter/ZliczaczLiter.java
@@ -5,19 +5,19 @@ class ZliczaczLiter {
         Scanner in = new Scanner(System.in);
         int N = Integer.parseInt(in.nextLine());
         int[] letterCounter = new int[52];
-        char[] alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         for (int i = 0; i < N; i++) {
             String str = in.nextLine();
             for (int j = 0; j < 52; j++) {
                 for (int k = 0; k < str.length(); k++) {
-                    if (str.charAt(k) == alphabet[j])
+                    if (str.charAt(k) == alphabet.charAt(j))
                         letterCounter[j]++;
                 }
             }
         }
         for (int i = 0; i < 52; i++) {
             if (letterCounter[i] > 0)
-                System.out.println(alphabet[i] + " " + letterCounter[i]);
+                System.out.println(alphabet.charAt(i) + " " + letterCounter[i]);
         }
     }
 }
